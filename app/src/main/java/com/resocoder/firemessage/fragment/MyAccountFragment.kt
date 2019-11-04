d28cfbf3ad71c2dc96d55c5a45f3f478a5de5194
John <johndelacruzm@gmail.com>
2019-11-04 11:54:48 -0500

    Update MyAccountFragment.kt

--- app/src/main/java/com/resocoder/firemessage/fragment/MyAccountFragment.kt
+++ app/src/main/java/com/resocoder/firemessage/fragment/MyAccountFragment.kt
@@ -10,6 +10,7 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import com.bumptech.glide.Glide
 import com.firebase.ui.auth.AuthUI
 
 import com.resocoder.firemessage.R
